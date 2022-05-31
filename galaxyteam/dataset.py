768d130673844fc571681001d5ef732f2c4ddaed
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2022-05-31 10:48:06 -0400

    Adds image resizing function

--- galaxyteam/dataset.py
+++ galaxyteam/dataset.py
@@ -5,6 +5,9 @@ analysis and cleaning it.
 
 import os
 import kaggle
+from skimage.io import imread, imsave
+from skimage import img_as_ubyte
+from skimage.transform import resize
 
 
 def download_dataset(savedir='./data/'):
@@ -24,3 +27,21 @@ def download_dataset(savedir='./data/'):
                                       path=savedir)
 
     print('done.')
+
+
+def resize_image(read_fp, save_fp, resize_shape=(1100, 1250, 3)):
+    """
+    Reads an image, converts it to uint8, resizes it, and saves it to file.
+
+    Args:
+        read_fp (string): The filepath to read the image from.
+        save_fp (string): The filepath to save the image to.
+        resize_shape (iter): The dimensions of the final saved image.
+    """
+
+    # Make the save folder if it doesn't already exist.
+    os.makedirs(os.path.dirname(save_fp), exist_ok=True)
+
+    image = imread(read_fp)
+    resized = resize(image, resize_shape)
+    imsave(save_fp, img_as_ubyte(resized))
