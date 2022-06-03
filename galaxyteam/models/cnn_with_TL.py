1b2d955e5a281fde1e930bb0a6d0a8ddbc25a2a7
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2022-06-03 17:47:51 -0400

    Fixed lots of linter issues

--- galaxyteam/models/cnn_with_TL.py
+++ galaxyteam/models/cnn_with_TL.py
@@ -8,7 +8,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 # pylint: disable=[E0611,E0401]
 from tensorflow.keras.metrics import Recall, Precision
-from tensorflow.keras.layers import (Dense, Dropout, Input, GlobalAveragePooling2D)
+from tensorflow.keras.layers import (Dense, Dropout, Input,
+                                     GlobalAveragePooling2D)
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.applications.resnet_v2 import ResNet152V2
 from tensorflow import keras
@@ -18,8 +19,7 @@ from galaxyteam.models.cnn import create_dataset, get_tf_train_val
 from galaxyteam.metrics import F1_Score
 
 
-
-def build_TL(finetune = False):
+def build_TL(finetune=False):
     '''
     Builds the Transfer learning based CNN model
     Returns:
@@ -27,37 +27,36 @@ def build_TL(finetune = False):
     '''
 
     IMG_SIZE = 224
-    ## We are using ResNet as out base model
+    # We are using ResNet as out base model
     base_model = ResNet152V2(
                             weights='imagenet',
                             input_shape=(IMG_SIZE, IMG_SIZE, 3),
                             include_top=False)
     base_model.trainable = finetune
-    #Input shape = [width, height, color channels]
+    # Input shape = [width, height, color channels]
     inputs = Input(shape=(IMG_SIZE, IMG_SIZE, 3))
     x = base_model(inputs)
     # Head
     x = GlobalAveragePooling2D()(x)
     x = Dense(128, activation='relu')(x)
     x = Dropout(0.1)(x)
-    #Final Layer (Output)
+    # Final Layer (Output)
     output = Dense(1, activation='sigmoid')(x)
     model = keras.Model(inputs=[inputs], outputs=output)
     keras.backend.clear_session()
 
     model.compile(loss='binary_crossentropy',
-                optimizer = keras.optimizers.Adam(),
-                metrics=[Recall(),
-                Precision(),
-                F1_Score()])
+                  optimizer=keras.optimizers.Adam(),
+                  metrics=[Recall(),
+                           Precision(),
+                           F1_Score()])
     return model
 
 
-
-def train_TL(finetune = False, epochs=10, batch_size=32, val_frac=0.2,
-              train_info_file_path=(Path('data')
-                                    .joinpath('preprocessed',
-                                              'train_metadata.csv'))):
+def train_TL(finetune=False, epochs=10, batch_size=32, val_frac=0.2,
+             train_info_file_path=(Path('data')
+                                   .joinpath('preprocessed',
+                                             'train_metadata.csv'))):
     """
     Generates the necessary datasets and trains the TL based CNN.
     Args:
