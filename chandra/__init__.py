36aefd018d35f7c1a5215574e7c4b85a6f0cdd3d
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2022-06-04 15:20:16 -0400

    Changed package name

---
+++ chandra/__init__.py
@@ -0,0 +1,9 @@
+"""
+chandra
+
+Classifies x-rays as pneumonia or not using k-NN and convolutional neural
+networks.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Adam Broussard, Shreeya Behera, Karthik Prabhu'
